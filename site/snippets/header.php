10a2fb4b61f714f323ea0bcb9b5710a46f6bcec8
vitrinekast <roosschuurmans@gmail.com>
2022-03-16 17:24:55 +0100

    add analytics

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -38,6 +38,27 @@
     <?= css(['assets/style.css?v=' . date('h:i:sa') ,'@auto']) ?>
     <?= css('assets/css/print.css', 'print') ?>
 
+
+    <!-- Matomo -->
+    <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u = "//vitrinekast.xyz/analytics/";
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d = document,
+            g = d.createElement('script'),
+            s = d.getElementsByTagName('script')[0];
+        g.async = true;
+        g.src = u + 'matomo.js';
+        s.parentNode.insertBefore(g, s);
+    })();
+    </script>
+    <!-- End Matomo Code -->
+
 </head>
 
 <body>
