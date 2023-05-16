54ee3757b86a9a992155babc3047b134b7512229
vitrinekast <roosschuurmans@gmail.com>
2023-05-16 10:00:22 +0200

    tag

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -7,7 +7,18 @@
 <?php
   // echo js(['assets/js/libs.js']);
   echo js([ 'assets/js/app.js', '@auto'])
+  
 ?>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6FM1YBVTSX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6FM1YBVTSX');
+</script>
 
 
 
